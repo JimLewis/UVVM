f2b01f9e6f3722ffdff68bd9dbe2c63d0d743317
Jim Lewis <jim@synthworks.com>
2026-07-24 12:04:29 -0700

    Added copyright and modification notice

--- bitvis_uart/tb/uart_vvc_demo_tb.vhd
+++ bitvis_uart/tb/uart_vvc_demo_tb.vhd
@@ -1,5 +1,6 @@
 --================================================================================================================================
 -- Copyright 2024 UVVM
+-- Copyright 2026 SynthWorks Design Inc
 -- Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 and in the provided LICENSE.TXT.
 --
@@ -9,9 +10,18 @@
 --================================================================================================================================
 -- Note : Any functionality not explicitly described in the documentation is subject to change at any time
 ----------------------------------------------------------------------------------------------------------------------------------
-
 ------------------------------------------------------------------------------------------
 -- Description   : See library quick reference (under 'doc') and README-file(s)
+------------------------------------------------------------------------------------------
+-- Modifications:
+--   July 2026  done by SynthWorks
+--       This file is modified from the UVVM Master branch
+--       Minor modifications were done to add
+--       * OSVVM library and context
+--       * Constant C_TESTCASE_FILE_PATH
+--       * Calls to SetTestName, TranscriptOpen, and SetTranscriptMirror
+--       * Calls to OSVVM TranscriptClose, AffirmIfTranscriptsMatch, and EndOfTestReports
+--       UVVM calls to report_alert_counters were changed to INTERMEDIATE (directly or indirectly)
 ------------------------------------------------------------------------------------------
 
 library IEEE;
@@ -268,7 +278,7 @@ begin
     -----------------------------------------------------------------------------
     -- Ending the simulation
     -----------------------------------------------------------------------------
-    await_uvvm_completion(1000 ns, print_alert_counters => REPORT_ALERT_COUNTERS, scope => C_SCOPE);
+    await_uvvm_completion(1000 ns, print_alert_counters => REPORT_ALERT_COUNTERS, scope => C_SCOPE); -- OSVVM
     log(ID_LOG_HDR, "SIMULATION COMPLETED", C_SCOPE);
 
     -- OSVVM Test Completion Steps
