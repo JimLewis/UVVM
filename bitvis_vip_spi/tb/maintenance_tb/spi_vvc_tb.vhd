930a535079af83daad493d47e3f321a662ecc21b
Jim Lewis <jim@synthworks.com>
2026-07-24 21:10:46 -0700

    Added modification and copyright notice

--- bitvis_vip_spi/tb/maintenance_tb/spi_vvc_tb.vhd
+++ bitvis_vip_spi/tb/maintenance_tb/spi_vvc_tb.vhd
@@ -1,5 +1,6 @@
 --================================================================================================================================
 -- Copyright 2024 UVVM
+-- Copyright 2026 SynthWorks Design Inc
 -- Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 and in the provided LICENSE.TXT.
 --
@@ -12,6 +13,11 @@
 
 ------------------------------------------------------------------------------------------
 -- Description   : See library quick reference (under 'doc') and README-file(s)
+------------------------------------------------------------------------------------------
+-- Modifications:
+--   July 2026  done by SynthWorks
+--       This file is modified from the UVVM Master branch
+--       Added open_cores_spi library
 ------------------------------------------------------------------------------------------
 
 library ieee;
